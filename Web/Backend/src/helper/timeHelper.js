da27b33a178179bf562bb1b2bca6b50da3eff94c
topdev1228 <topdeveloper1228@gmail.com>
2020-07-13 08:58:30 -0600

    added: get owner list api, add company api

---
+++ Web/Backend/src/helper/timeHelper.js
@@ -0,0 +1,44 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const s3buckets = require('../constants/s3buckets')
+var md5 = require('md5');
+var fs = require('fs')
+
+var AWS = require('aws-sdk');
+s3 = new AWS.S3({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
+});
+
+s3_functions = {
+    uploadS3:uploadS3
+}
+
+function uploadS3(files, bucket){
+    return new Promise(function (resolve, reject) {
+        filename_splits = files.avatar.name.split('.');
+        fs.readFile(files.avatar.path, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                let params = {
+                    Bucket: bucket,
+                    Key: md5(Date.now()) + "." + filename_splits[filename_splits.length - 1],
+                    Body: data,
+                    ContentType: files.avatar.mimetype,
+                    ACL: 'public-read'
+                };
+
+                s3.upload(params, function (error, response) {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve(response)
+                    }
+                });
+            }
+        })
+    })
+}
+
+module.exports = s3_functions
---
+++ Web/Backend/src/services/web/admin/owner-service.js
@@ -0,0 +1,160 @@
+/**
+ * Auth service file
+ * 
+ * @package   backend/src/services
+ * @author    Taras Hryts <streaming9663@gmail.com>
+ * @copyright 2020 Say Digital Company
+ * @license   Turing License
+ * @version   2.0
+ * @link      https://turing.ly/api/auth/
+ */
+
+var managerModel = require('../../../models/web/manager-model')
+var jwt = require('jsonwebtoken')
+var message = require('../../../constants/message')
+var code = require('../../../constants/code')
+var key = require('../../../config/key-config')
+var timer  = require('../../../constants/timer')
+
+var managerService = {
+  getCompanyBuilding: getCompanyBuilding,
+  getManagerList: getManagerList,
+  createManager: createManager,
+  getManager: getManager,
+  updateManager: updateManager,
+}
+
+
+/**
+ * Function that get company and buildling list
+ *
+ * @author  Taras Hryts <streaming9663@gmail.com>
+ * @param   object authData
+ * @return  json 
+ */
+function getCompanyBuilding(uid) {
+  return new Promise((resolve, reject) => {
+    managerModel.getCompanyListByUser(uid).then((companyList) => {
+        if (companyList) {
+          managerModel.getBuildingListByUser(uid).then((buildingList) => {
+            let token = jwt.sign({ uid: uid }, key.JWT_SECRET_KEY, {
+              expiresIn: timer.TOKEN_EXPIRATION
+            })
+            resolve({ code: code.OK, message: '', data: { 'token': token, 'companylist': companyList, 'buildinglist': buildingList } })
+          })
+          
+        }
+      }).catch((err) => {
+        if (err.message === message.INTERNAL_SERVER_ERROR)
+          reject({ code: code.INTERNAL_SERVER_ERROR, message: err.message, data: {} })
+        else
+          reject({ code: code.BAD_REQUEST, message: err.message, data: {} })
+      })
+  })
+}
+
+/**
+ * Function that get building list
+ *
+ * @author  Taras Hryts <streaming9663@gmail.com>
+ * @param   object authData
+ * @return  json 
+ */
+function getManagerList(uid, data) {
+    return new Promise((resolve, reject) => {
+      managerModel.getManagerList(uid, data).then((managerList) => {
+          if (managerList) {
+            managerModel.getCountManagerList(uid, data).then((managerCount) => {
+              let token = jwt.sign({ uid: uid }, key.JWT_SECRET_KEY, {
+                expiresIn: timer.TOKEN_EXPIRATION
+              })
+              resolve({ code: code.OK, message: '', data: { 'token': token, 'totalpage': Math.ceil(managerCount / Number(data.row_count)), 'managerlist': managerList, 'totalcount': managerCount} })
+            })
+            
+          }
+        }).catch((err) => {
+          if (err.message === message.INTERNAL_SERVER_ERROR)
+            reject({ code: code.INTERNAL_SERVER_ERROR, message: err.message, data: {} })
+          else
+            reject({ code: code.BAD_REQUEST, message: err.message, data: {} })
+        })
+    })
+  }
+
+/**
+ * Function that create manager
+ *
+ * @author  Taras Hryts <streaming9663@gmail.com>
+ * @param   object authData
+ * @return  json 
+ */
+function createManager(uid, data, file_name) {
+  return new Promise((resolve, reject) => {
+    managerModel.createManager(uid, data, file_name).then((data) => {
+      if (data) {
+        let token = jwt.sign({ uid: uid }, key.JWT_SECRET_KEY, {
+          expiresIn: timer.TOKEN_EXPIRATION
+        })
+        
+        resolve({ code: code.OK, message: '', data: { 'token': token} })
+      }
+    }).catch((err) => {
+      if (err.message === message.INTERNAL_SERVER_ERROR)
+        reject({ code: code.INTERNAL_SERVER_ERROR, message: err.message, data: {} })
+      else if (err.message === message.MANAGER_ALREADY_EXIST) 
+        reject({ code: code.ALREADY_EXIST, message: err.message, data: {} })
+      else
+        reject({ code: code.BAD_REQUEST, message: err.message, data: {} })
+    })
+  })
+}
+
+/**
+ * Function that get manager
+ *
+ * @author  Taras Hryts <streaming9663@gmail.com>
+ * @param   object authData
+ * @return  json 
+ */
+function getManager(uid, id) {
+  return new Promise((resolve, reject) => {
+    managerModel.getManager(id).then((manager) => {
+      let token = jwt.sign({ uid: uid }, key.JWT_SECRET_KEY, {
+        expiresIn: timer.TOKEN_EXPIRATION
+      })
+      resolve({ code: code.OK, message: '', data: { 'token': token, 'manager':  manager.manager, 'buildinglist': manager.buildingList} })
+      }).catch((err) => {
+        if (err.message === message.INTERNAL_SERVER_ERROR)
+          reject({ code: code.INTERNAL_SERVER_ERROR, message: err.message, data: {} })
+        else
+          reject({ code: code.BAD_REQUEST, message: err.message, data: {} })
+      })
+  })
+}
+
+/**
+ * Function that update manager
+ *
+ * @author  Taras Hryts <streaming9663@gmail.com>
+ * @param   object authData
+ * @return  json 
+ */
+function updateManager(uid, id, data) {
+  return new Promise((resolve, reject) => {
+    managerModel.updateManager(id, data).then((result) => {
+        if (result) {
+          let token = jwt.sign({ uid: uid }, key.JWT_SECRET_KEY, {
+            expiresIn: timer.TOKEN_EXPIRATION
+          })
+          
+          resolve({ code: code.OK, message: '', data: { 'token': token } })
+        }
+      }).catch((err) => {
+        if (err.message === message.INTERNAL_SERVER_ERROR)
+          reject({ code: code.INTERNAL_SERVER_ERROR, message: err.message, data: {} })
+        else
+          reject({ code: code.BAD_REQUEST, message: err.message, data: {} })
+      })
+  })
+}
+module.exports = managerService
