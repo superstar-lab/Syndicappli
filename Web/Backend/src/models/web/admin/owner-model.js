c5024277fc9434e677958eb42a9e92618d7b837b
topdev1228 <topdeveloper1228@gmail.com>
2020-07-23 08:31:00 +0300

    modify: fix the get owner list issue on admin

--- Web/Backend/src/models/web/admin/owner-model.js
+++ Web/Backend/src/models/web/admin/owner-model.js
@@ -47,7 +47,7 @@ function getOwnerList(uid, data) {
                     LEFT JOIN buildings ON user_relationship.relationID = buildings.buildingID 
                     Left join companies using (companyID)
                     LEFT JOIN ( SELECT count( buildingID ) count, buildingID, userID FROM apartments LEFT JOIN buildings USING ( buildingID ) GROUP BY apartments.buildingID, apartments.userID ) s ON buildings.buildingID = s.buildingID and users.userID = s.userID
-                    WHERE users.usertype = "owner" and users.firstname like ? and users.permission = ? `
+                    WHERE users.usertype = "owner" and users.firstname like ? and users.permission = ? and s.count > 0 `
 
         sort_column = Number(data.sort_column);
         row_count = Number(data.row_count);
@@ -114,7 +114,7 @@ function getCountOwnerList(uid, data) {
                     LEFT JOIN buildings ON user_relationship.relationID = buildings.buildingID 
                     Left join companies using (companyID)
                     LEFT JOIN ( SELECT count( buildingID ) count, buildingID, userID FROM apartments LEFT JOIN buildings USING ( buildingID ) GROUP BY apartments.buildingID, apartments.userID ) s ON buildings.buildingID = s.buildingID and users.userID = s.userID
-                    WHERE users.usertype = "owner" and users.firstname like ? and users.permission = ? `
+                    WHERE users.usertype = "owner" and users.firstname like ? and users.permission = ? and s.count > 0 `
         let params = [search_key, data.status];
         if (data.role !== "all") {
             query += 'and users.owner_role = ? ';
