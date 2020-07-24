35989f31299ec569e94d3b682a21c21c3fea0188
topdev1228 <topdeveloper1228@gmail.com>
2020-07-24 16:02:44 +0800

    set the success message when email is sent

--- Web/Admin/src/views/WebAppAdmin/ForgotPassword/ForgotPassword.js
+++ Web/Admin/src/views/WebAppAdmin/ForgotPassword/ForgotPassword.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { ToastsContainer, ToastsContainerPosition, ToastsStore } from 'react-toasts';
 import { withRouter } from 'react-router-dom';
 import MyButton from 'components/MyButton';
 import TextField from '@material-ui/core/TextField';
 import useStyles from './useStyles';
-import useGlobal from 'Global/global';
 import AdminService from '../../../services/api.js';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import Link from '@material-ui/core/Link';
 
 const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
 const validateForm = (errors) => {
@@ -19,12 +17,16 @@ const validateForm = (errors) => {
   return valid;
 }
 const ForgotPassword = (props) => {
-  const [globalState, globalActions] = useGlobal();
   const { history } = props;
   const [visibleIndicator, setVisibleIndicator] = React.useState(false);
   const classes = useStyles();
   const [email, setEmail] = useState('');
   const [errorsEmail, setErrorsEmail] = useState('');
+  const [bodySide, setBodySide] = useState({
+    sendText: 'Envoyer',
+    bodyText: 'Mot de passe oublié',
+    bodyState: false
+  });
   const logo = {
     url: '/images/Login.png',
   };
@@ -37,32 +39,49 @@ const ForgotPassword = (props) => {
     setEmail(event.target.value);
     setErrorsEmail(errorsMail);
   }
+  useEffect(() => {
+    let params = new URLSearchParams(window.location.search);
+    if (params.get('success') === 'success') {
+      var data = {
+        sendText: 'Se connecter',
+        bodyText: 'Un email vient de vous être envoyé afin de réinitialiser votre mot de passe.',
+        bodyState: true
+      };
+      setBodySide(data);
+    }
+  }, []);
   const handleClickSend = () => {
-    let cnt = 0;
-    if (email.length === 0) { setErrorsEmail('please enter your eamil'); cnt++; }
-    if (cnt === 0) {
-      if (validateForm(errorsEmail)) {
-        var data = {};
-        data['email'] = email;
-        setVisibleIndicator(true);
-        AdminService.forgotPassword(data)
-          .then(
-            response => {
-              setVisibleIndicator(false);
-              if (response.data.code !== 200) {
-                ToastsStore.error(response.data.message);
-              } else {
-                ToastsStore.success(response.data.message);
-                history.push('/login');
+    if (!bodySide.bodyState) {
+      let cnt = 0;
+      if (email.length === 0) { setErrorsEmail('please enter your eamil'); cnt++; }
+      if (cnt === 0) {
+        if (validateForm(errorsEmail)) {
+          var data = {};
+          data['email'] = email;
+          setVisibleIndicator(true);
+          AdminService.forgotPassword(data)
+            .then(
+              response => {
+                setVisibleIndicator(false);
+                if (response.data.code !== 200) {
+                  ToastsStore.error(response.data.message);
+                } else {
+                  // ToastsStore.success(response.data.message);
+                  window.location.replace('/forgotpassword?success=' + 'success');
+                }
+              },
+              error => {
+                setVisibleIndicator(false);
+                ToastsStore.error("Can't connect to the Server!");
               }
-            },
-            error => {
-              setVisibleIndicator(false);
-              ToastsStore.error("Can't connect to the Server!");
-            }
-          );
+            );
+        }
+        else setErrorsEmail('Email is not valid!');
       }
-      else setErrorsEmail('Email is not valid!');
+    }
+    else {
+      history.push('/login');
+      window.location.reload();
     }
   }
   return (
@@ -74,46 +93,53 @@ const ForgotPassword = (props) => {
         <Grid item container justify="center">
           <img src={logo.url} className={classes.logo} alt="" />
         </Grid>
-        <Grid item container justify="center">
-          <p className={classes.title}>Mot de passe perdu? Veuillez saisir votre adresse e-mail. Vous recevrez un lien pour créer un nouveau mot de passe par e-mail. </p>
-        </Grid>
+        {
+          bodySide.bodyState === false ?
+            <Grid item container justify="center">
+              <p className={classes.title}>Mot de passe perdu? Veuillez saisir votre adresse e-mail. Vous recevrez un lien pour créer un nouveau mot de passe par e-mail. </p>
+            </Grid>
+            : <Grid item container justify="center" >
+              <div className={classes.title}></div>
+            </Grid>
+        }
         <Grid item container justify="center">
           <Grid item container xs={1} sm={2} md={4}></Grid>
+
           <Grid xs={10} sm={7} md={4} item container direction="column" className={classes.body}>
             <Grid item></Grid>
+            {
+              bodySide.bodyState === false ?
+              <Grid item>
             <Grid item container justify="center">
-              <p className={classes.boxTitle}><b>Mot de passe oublié</b></p>
+              <p className={classes.boxTitle}><b>{bodySide.bodyText}</b></p>
             </Grid>
-            <Grid item container className={classes.input}>
-              <Grid xs={1} item></Grid>
-              <Grid xs={10} item container direction="column" spacing={2}>
-                <Grid item><p className={classes.itemTitle}>Email</p></Grid>
-                <Grid item direction='column'>
-                  <TextField
-                    variant="outlined"
-                    fullWidth
-                    value={email}
-                    onChange={handleChangeEmail}
-                  />
-                  {errorsEmail.length > 0 &&
-                    <span className={classes.error}>{errorsEmail}</span>}
+
+                <Grid item container className={classes.input}>
+                  <Grid xs={1} item></Grid>
+                  <Grid xs={10} item container direction="column" spacing={2}>
+                    <Grid item><p className={classes.itemTitle}>Email</p></Grid>
+                    <Grid item container direction='column'>
+                      <TextField
+                        variant="outlined"
+                        fullWidth
+                        value={email}
+                        onChange={handleChangeEmail}
+                      />
+                      {errorsEmail.length > 0 &&
+                        <span className={classes.error}>{errorsEmail}</span>}
+                    </Grid>
+                  </Grid>
+                  <Grid xs={1} item></Grid>
                 </Grid>
+                </Grid>
+                : <Grid item container justify="center" style={{padding: 30}}>
+                  <Grid item/>
+                <Grid item>  <p className={classes.boxTitle}>{bodySide.bodyText}</p></Grid>
+                <Grid item/>
               </Grid>
-              <Grid xs={1} item></Grid>
-            </Grid>
+            }
             <Grid item container justify="center">
-              <MyButton name={"Envoyer"} color="1" onClick={handleClickSend} />
-            </Grid>
-          </Grid>
-          <Grid item container xs={1} sm={2} md={4}></Grid>
-        </Grid>
-        <Grid item container justify="center">
-          <Grid item container xs={1} sm={2} md={4}></Grid>
-          <Grid item container xs={10} sm={7} md={4}>
-            <Grid item container direction="row-reverse">
-              <Link href="/login" variant="body2">
-                <p className={classes.forgot}>Accéder à la connexion</p>
-              </Link>
+              <MyButton name={bodySide.sendText} color="1" onClick={handleClickSend} />
             </Grid>
           </Grid>
           <Grid item container xs={1} sm={2} md={4}></Grid>
