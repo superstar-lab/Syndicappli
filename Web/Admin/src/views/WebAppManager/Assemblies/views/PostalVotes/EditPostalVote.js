41c8455adf901825b49d888e4a86c3bfab47db5f
topdev1228 <topdeveloper1228@gmail.com>
2020-08-06 16:10:24 +0800

    implemented skeleton of assembly postal vote edit part.

---
+++ Web/Admin/src/views/WebAppManager/Assemblies/views/PostalVotes/EditPostalVote.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import authService from 'services/authService.js';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { ToastsContainer, ToastsContainerPosition, ToastsStore } from 'react-toasts';
+import { withRouter } from 'react-router-dom';
+import { EditPostalVoteStyles as useStyles } from './useStyles';
+import { Grid } from '@material-ui/core';
+import DecesionCard from './compoment/DecesionCard';
+const EditPostalVote = (props) => {
+    const classes = useStyles();
+    const { history } = props;
+    const accessAssemblies = authService.getAccess('role_assemblies');
+    const [visibleIndicator, setVisibleIndicator] = useState(false);
+
+    return (
+        <div className={classes.root}>
+            {
+                visibleIndicator ? <div className={classes.div_indicator}> <CircularProgress className={classes.indicator} /> </div> : null
+            }
+            <div className={classes.title}>
+            </div>
+            <div className={classes.body}>
+                <Grid item container direction="column" spacing={1} className={classes.editHeader}>
+                    <Grid item>
+                        <p className={classes.headerTitle}><b>Jean Dupont</b></p>
+                    </Grid>
+                    <Grid item>
+                        <p className={classes.subTitle}>7 rue Berlioz, 75012 Paris</p>
+                    </Grid>
+                    <Grid item>
+                        <p className={classes.subTitle}><b>Lots 126, 47</b></p>
+                    </Grid>
+                </Grid>
+
+                <DecesionCard
+                    decesion_number={1}
+                    decesion_name={'Installation clôture'}
+                    description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan mauris risus, ut tincidunt augue dictum eu. Donec molestie nibh purus, non sollicitudin nisl condimentum vitae. Suspendisse vehicula laoreet ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan mauris risus, ut tincidunt augue dictum eu. Donec molestie nibh purus, non sollicitudin nisl condimentum vitae. Suspendisse vehicula laoreet ullamcorper. '}
+                    calc_mode={'Majorité simple - Art. 24'}
+                    vote_branch={'Clef 1'}
+                    apartment_amount={'300/1500'}
+                    vote_result={'Pour'}
+                />
+
+                <DecesionCard
+                    decesion_number={2}
+                    decesion_name={'Peinture parking'}
+                    description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan mauris risus, ut tincidunt augue dictum eu. Donec molestie nibh purus, non sollicitudin nisl condimentum vitae. Suspendisse vehicula laoreet ullamcorper. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce accumsan mauris risus, ut tincidunt augue dictum eu. Donec molestie nibh purus, non sollicitudin nisl condimentum vitae. Suspendisse vehicula laoreet ullamcorper. '}
+                    calc_mode={'Majorité simple - Art. 25'}
+                    vote_branch={'Clef 2'}
+                    apartment_amount={'300/1500'}
+                    vote_result={'Pour'}
+                />
+            </div>
+            <ToastsContainer store={ToastsStore} position={ToastsContainerPosition.TOP_RIGHT} />
+        </div>
+    );
+};
+
+export default withRouter(EditPostalVote);
--- Web/Admin/src/views/WebAppManager/Assemblies/views/PostalVotes/useStyles.js
+++ Web/Admin/src/views/WebAppManager/Assemblies/views/PostalVotes/useStyles.js
@@ -159,56 +159,73 @@ export const AddPostalVoteStyles = makeStyles(theme => ({
     color: 'gray'
   },
 }));
-export const ResolutionsStyles = makeStyles(theme => ({
+export const EditPostalVoteStyles = makeStyles(theme => ({
   root: {
     [theme.breakpoints.up('xl')]: {
       marginTop: 64,
       marginBottom: 64,
-      padding: 40,
+      paddingTop: 58,
+      paddingBottom: 58,
       borderRadius: 30,
     },
     [theme.breakpoints.down('lg')]: {
       marginTop: 45,
       marginBottom: 45,
-      padding: 28,
+      paddingTop: 41,
+      paddingBottom: 41,
       borderRadius: 21,
     },
     [theme.breakpoints.down('md')]: {
       marginTop: 32,
       marginBottom: 32,
-      padding: 20,
+      paddingTop: 29,
+      paddingBottom: 29,
       borderRadius: 15,
     },
     boxShadow: '0 3px 5px 2px rgba(128, 128, 128, .3)',
+  },
+  editHeader: {
+    [theme.breakpoints.up('xl')]: {
+      paddingLeft: 58,
+      paddingRight: 58,
+      marginBottom: 64,
+    },
+    [theme.breakpoints.down('lg')]: {
+      paddingLeft: 41,
+      paddingRight: 41,
+      marginBottom: 45,
+    },
+    [theme.breakpoints.down('md')]: {
+      paddingLeft: 29,
+      paddingRight: 29,
+      marginBottom: 32,
+    },
   },
   title:{
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2)
   },
-  doc_tip:{
+  headerTitle:{
     [theme.breakpoints.up('xl')]: {
-      fontSize :13
+      fontSize :32
     },
     [theme.breakpoints.down('lg')]: {
-      fontSize :9
+      fontSize :22
     },
     [theme.breakpoints.down('md')]: {
-      fontSize :6
+      fontSize :15
     },
-    textAlign: 'center'
   },
-  doc_name:{
+  subTitle:{
     [theme.breakpoints.up('xl')]: {
-      fontSize :18
+      fontSize :16
     },
     [theme.breakpoints.down('lg')]: {
-      fontSize :13
+      fontSize :11
     },
     [theme.breakpoints.down('md')]: {
-      fontSize :9
+      fontSize :8
     },
-    textAlign: 'center',
-    color: 'gray',
   },
   close: {
     cursor: 'pointer',
@@ -245,119 +262,10 @@ export const ResolutionsStyles = makeStyles(theme => ({
   indicator: {
     color: 'gray'
   },
-  documents: {
-    cursor: 'pointer',
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    border: '1px solid rgba(112,112,112,0.43)',
-    borderRadius: 8,
-    [theme.breakpoints.up('xl')]: {
-      width: 152,
-      height: 120,
-      marginTop: 20,
-    },
-    [theme.breakpoints.down('lg')]: {
-      width: 106,
-      height: 84,
-      marginTop: 14,
-    },
-    [theme.breakpoints.down('md')]: {
-      width: 74,
-      height: 59,
-      marginTop: 10,
-    },
-  },
-  sizepng: {
-    [theme.breakpoints.up('xl')]: {
-      width: 152,
-      height: 120,
-    },
-    [theme.breakpoints.down('lg')]: {
-      width: 106,
-      height: 84,
-    },
-    [theme.breakpoints.down('md')]: {
-      width: 74,
-      height: 59,
-    },
-  },
-  size: {
-    [theme.breakpoints.up('xl')]: {
-      width: 53,
-      height: 65,
-    },
-    [theme.breakpoints.down('lg')]: {
-      width: 37,
-      height: 46,
-    },
-    [theme.breakpoints.down('md')]: {
-      width: 26,
-      height: 32,
-    },
-  },
-  uploadHelp: {
-    [theme.breakpoints.up('xl')]: {
-      fontSize: 20,
-      width: 330,
-      height: 58,
-    },
-    [theme.breakpoints.down('lg')]: {
-      fontSize: 14,
-      width: 231,
-      height: 41,
-    },
-    [theme.breakpoints.down('md')]: {
-      fontSize: 10,
-      width: 162,
-      height: 29,
-    },
-    color: 'gray',
-    textAlign: 'center'
-  },
-  plus:{
-    color: 'rgb(54,54,54,0.25)',
-    [theme.breakpoints.up('xl')]: {
-      width:88 , 
-      height: 88,
-    },
-    [theme.breakpoints.down('lg')]: {
-      width:62 , 
-      height: 62,
-    },
-    [theme.breakpoints.down('md')]: {
-      width:43 , 
-      height: 43,
-    },
-  },
-  img: {
-    cursor: 'pointer',
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    border: '1px dashed rgba(112,112,112,0.43)',
-    borderRadius: 8,
-    [theme.breakpoints.up('xl')]: {
-      width: 330,
-      height: 265,
-    },
-    [theme.breakpoints.down('lg')]: {
-      width: 231,
-      height: 186,
-    },
-    [theme.breakpoints.down('md')]: {
-      width: 162,
-      height: 130,
-    },
-  },
-  input: {
-    display: 'none',
-    '&: hover':{
-      color: '#f00',
-	borderColor: '#f00',
-	borderStyle: 'solid',
-	boxShadow: 'inset 0 3px 4px #888',
-    }
-  }, 
+  
+  line: {
+    backgroundColor: '#E8E8E8',
+    height: 1
+}
 }));
   export default useStyles;
