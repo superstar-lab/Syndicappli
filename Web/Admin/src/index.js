c23d1de8d97a57347698c559ef9234a92e761f1f
topdev1228 <topdeveloper1228@gmail.com>
2020-08-11 05:18:04 +0800

    solved issue  'Unhandled Rejection (SecurityError): The operation is insecure.' in firefox

--- Web/Admin/src/index.js
+++ Web/Admin/src/index.js
@@ -5,4 +5,4 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
+serviceWorker.register();
