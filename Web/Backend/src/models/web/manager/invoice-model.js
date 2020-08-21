d75ff63ddb39cfbf44b315d628391654c01c5c1e
topdev1228 <topdeveloper1228@gmail.com>
2020-08-20 22:27:08 -0700

    modify: fix the invoice issue

--- Web/Backend/src/models/web/manager/invoice-model.js
+++ Web/Backend/src/models/web/manager/invoice-model.js
@@ -49,10 +49,10 @@ function getInvoiceOrder(data) {
                             if (o.discount_type = "fixed", 
                             (o.apartment_amount * o.price * (100 + o.vat_fee) / 100 - o.discount_amount) * 100 / (100 + o.vat_fee), 
                             (o.apartment_amount * o.price * (100 + o.vat_fee) / 100 * (100 - o.discount_amount) / 100) * 100 / (100 + o.vat_fee)), 2) total_amount, 
-                            ROUND(
-                                if (o.discount_type = "fixed", 
-                                (o.apartment_amount * o.price * (100 + o.vat_fee) / 100 - o.discount_amount) * 100 / (100 + o.vat_fee), 
-                                (o.apartment_amount * o.price *(100 + o.vat_fee) / 100 * (100 - o.discount_amount) / 100) * 100 / (100 + o.vat_fee)) / o.apartment_amount, 2) price
+                        ROUND(
+                            if (o.discount_type = "fixed", 
+                            (o.apartment_amount * o.price * (100 + o.vat_fee) / 100 - o.discount_amount) * 100 / (100 + o.vat_fee), 
+                            (o.apartment_amount * o.price *(100 + o.vat_fee) / 100 * (100 - o.discount_amount) / 100) * 100 / (100 + o.vat_fee)) / o.apartment_amount, 2) price
                     FROM
                         orders o
                         LEFT JOIN products p ON o.productID = p.productID
@@ -193,7 +193,7 @@ function downloadInvoiceAddon(data, res) {
  */
 function downloadInvoiceOrder(data, res) {
     return new Promise((resolve, reject) => {
-        let query = `Select c.name name, c.address address, c.email email, o.orderID invoice_number, o.start_date invoice_date, o.orderID order_id, o.start_date order_date, p.name product_name, o.apartment_amount amount_lot, o.price price, o.start_date date, 
+        let query = `Select c.name name, c.address address, c.email email, o.orderID invoice_number, o.start_date invoice_date, o.orderID order_id, o.start_date order_date, p.name product_name, o.apartment_amount amount_lot, o.start_date date, 
                      ROUND(if (o.discount_type = "fixed", 
                         if (o.vat_option = "true", o.price * o.apartment_amount * (100 + o.vat_fee) / 100, o.price * o.apartment_amount) - o.discount_amount,
                         if (o.vat_option = "true", o.price * o.apartment_amount * (100 + o.vat_fee) / 100, o.price * o.apartment_amount) * (100 - o.discount_amount) / 100
@@ -202,6 +202,10 @@ function downloadInvoiceOrder(data, res) {
                             (o.price * o.apartment_amount * (100 + o.vat_fee) / 100 - o.discount_amount) / ((100 + o.vat_fee)) * o.vat_fee,
                             (o.price * o.apartment_amount * (100 + o.vat_fee) / 100 / 100 * (100 - o.discount_amount)) / ((100 + o.vat_fee)) * o.vat_fee
                         ), 2) vat_amount,
+                    ROUND(
+                        if (o.discount_type = "fixed", 
+                        (o.apartment_amount * o.price * (100 + o.vat_fee) / 100 - o.discount_amount) * 100 / (100 + o.vat_fee), 
+                        (o.apartment_amount * o.price *(100 + o.vat_fee) / 100 * (100 - o.discount_amount) / 100) * 100 / (100 + o.vat_fee)) / o.apartment_amount, 2) price,
                     if (o.payment_method = "credit_card", "carte_bancaire", "SEPA") payment_method
                      from orders o left join companies c on o.companyID = c.companyID left join products p on o.productID = p.productID where o.orderID = ?`
         db.query(query, [data.orderID], (error, rows, fields) => {
