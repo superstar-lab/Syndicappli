4e19222073e9a2a3c640eb033a17c3df1a771c7d
topdev1228 <topdeveloper1228@gmail.com>
2020-08-24 11:45:34 -0700

    modify: fix the addon model without payment

--- Web/Backend/src/models/web/manager/addon-model.js
+++ Web/Backend/src/models/web/manager/addon-model.js
@@ -138,13 +138,18 @@ function buyAddon(uid, data) {
                                     reject({ message: message.NOT_USE_THIS_DISCOUNT_CODE})
                                 else {
                                     let query = `Select count(*) count from ` + table.ORDERS + ` where discount_codeID = ? and (permission = "active" or permission = "trash") and buyerID = ? and buyer_type = ?`
-                                    db.query(query, [data.discount_codeID, data.companyID, "buildings"], (error, rows, fields) => {
+                                    db.query(query, [data.discount_codeID, data.companyID, "buildings"], async (error, rows, fields) => {
                                         if (error)
                                             reject({ message: message.INTERNAL_SERVER_ERROR })
                                         else {
                                             if (data.discount_codeID > 0 && amount_of_use_per_user != -1 && rows[0].count + 1 > amount_of_use_per_user)
                                                 reject({ message: message.NOT_USE_THIS_DISCOUNT_CODE })
                                             else {
+                                                var response = await stripeHelper.createBankSource(data.account_IBAN, data.account_holdername)
+                                                data.stripe_sourceID = response.id
+                                                building_response = await adminBuildingModel.getBuilding(data.buildingID)
+
+                                                await stripeHelper.attachSourceToCustomer(building_response.building[0].customer_id, data.stripe_sourceID)
                                                 createCharge(data).then((response) => {
                                                     let query = `Insert into ` + table.ORDERS + ` (buyer_type, productID, companyID, buildingID, buyerID, buyer_name, billing_cycle, renewal, price_type, price, vat_option, vat_fee, apartment_amount, start_date, end_date, payment_method, discount_codeID, discount_type, discount_amount, status, permission, created_by, created_at) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
                                                     db.query(query, ["buildings", data.productID, data.companyID, data.buildingID, data.buyerID, data.buyer_name, data.billing_cycle, data.renewal, data.price_type, data.price, data.vat_option, data.vat_fee, data.apartment_amount, timeHelper.getCurrentDate(), timeHelper.getNextYearDate(), data.payment_method, data.discount_codeID, data.discount_type, data.discount_amount, "active", "active", uid, timeHelper.getCurrentTime()], function (error, result, fields) {
