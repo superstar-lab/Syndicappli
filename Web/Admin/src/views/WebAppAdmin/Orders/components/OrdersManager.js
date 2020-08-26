41d52db42d35090f9264c3290803a300e0e0d860
topdev1228 <topdeveloper1228@gmail.com>
2020-08-26 14:31:29 +0800

    added id to file name when invoice is downloaded

--- Web/Admin/src/views/WebAppAdmin/Orders/components/OrdersManager.js
+++ Web/Admin/src/views/WebAppAdmin/Orders/components/OrdersManager.js
@@ -167,7 +167,7 @@ const OrdersManager = (props) => {
           const downloadUrl = window.URL.createObjectURL(new Blob([data]));
           const link = document.createElement('a');
           link.href = downloadUrl;
-          link.setAttribute('download', 'Invoice(' + date + ').pdf');
+          link.setAttribute('download', 'Invoice' + id + '(' + date + ').pdf');
           document.body.appendChild(link);
           link.click();
           link.remove();
